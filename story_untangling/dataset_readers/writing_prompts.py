4ea02066cbbbd925748f047d58545c415a0cb76e
David Wilmot <david.wilmot@ed.ac.uk>
2018-12-05 18:24:52 +0000

    Initial change to using a sqllite database for sentence ordering.

--- story_untangling/dataset_readers/writing_prompts.py
+++ story_untangling/dataset_readers/writing_prompts.py
@@ -1,5 +1,9 @@
+import asyncio
 import json
 import logging
+import os
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from pathlib import Path
 from typing import Dict
 
 import spacy
@@ -15,11 +19,18 @@ from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
 from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter, SpacyWordSplitter
 from allennlp.data.tokenizers.sentence_splitter import SpacySentenceSplitter
+from aiofile import AIOFile, LineReader
+import aiofile
+import dataset
+
 from overrides import overrides
+from sqlalchemy import Index
 
 from story_untangling.dataset_readers.dataset_utils import dual_window
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+import multiprocessing as mp
 
 
 @DatasetReader.register("writing_prompts")
@@ -50,6 +61,12 @@ class WritingPromptsDatasetReader(DatasetReader):
         Whether to yield the target predictive context or not.
     target_negative : bool, (optional, default=True)
         Whether to yield a negative target context or not.
+    dataset_path : str, (optional, default=./dataset-cache/)
+        Path for caching the dataset features. Will reuse rather than rebuilding if it finds the SQLLite dataset.
+    use_existing_cached_db : bool, (optional, default=True)
+        Reused an existing cached database db if it exists or delete and recreate.
+    db_discriminator : str (optional, default=def)
+        Allow multiple databases to be kept separate rather than wiping over each other.
     """
     def __init__(self,
                  source_tokenizer: Tokenizer = None,
@@ -60,6 +77,9 @@ class WritingPromptsDatasetReader(DatasetReader):
                  sentence_context_window: int = 2,
                  target_positive: bool = True,
                  target_negative: bool = True,
+                 dataset_path: str = "./dataset-cache/",
+                 use_existing_cached_db: bool = True,
+                 db_discriminator = "def",
                  lazy: bool = False) -> None:
         super().__init__(lazy)
         self._source_tokenizer = source_tokenizer or WordTokenizer()
@@ -71,24 +91,35 @@ class WritingPromptsDatasetReader(DatasetReader):
         self._target_positive: bool = target_positive
         self._target_negative: bool = target_negative
         self._sentence_splitter = SpacySentenceSplitter()
+        self._dataset_path = dataset_path
+        self._use_existing_cached_db = use_existing_cached_db
+        self._db_discriminator = db_discriminator
 
     @overrides
     def _read(self, file_path):
-        with open(cached_path(file_path), "r") as data_file:
-            logger.info(f"WritingPromptsDatasetReader: Reading instances from lines in file at: {file_path}", file_path)
-            for line_num, line in enumerate(data_file):
-                line = line.strip("\n")
 
-                if not line:
-                    continue
+        loop = asyncio.get_event_loop()
+        dataset_db = loop.run_until_complete(create_dataset_db(self._dataset_path, self._db_discriminator, file_path, self._use_existing_cached_db))
 
-                line = line.replace("<newline>","") # The newline isn't relevant, it's just Reddit formatting.
-                sentences = self._sentence_splitter.split_sentences(line)
-                for source_sequence, target_sequence in dual_window(sentences, size=self._sentence_context_window):
-                    source_sequence = " ".join(source_sequence)
-                    logging.debug(f"Source: '{source_sequence}', Target: '{target_sequence}'")
-                    yield self.text_to_instance(source_sequence, target_sequence)
 
+        db = dataset.connect(dataset_db, engine_kwargs={"pool_recycle": 3600})
+
+        stories = db.query('SELECT * FROM story ORDER BY id')
+        for story in stories:
+            story_id = story["id"]
+            # Id will be the same as the sentence num as they are inserted as a batch in sequence.
+            sentences = [s for s in db.query(f'SELECT * FROM sentence WHERE story_id = {story_id} ORDER BY id')]
+            if len(sentences) == 0:
+                logging.warning(f"Story has no sentences: {story_id}")
+                continue
+
+            sentence_text  = [s["text"] for s in sentences]
+            sentence_lengths = [ s["sentence_len"] for s in sentences]
+
+            for source_sequence, target_sequence in dual_window(sentence_text, size=self._sentence_context_window):
+                source_sequence = " ".join(source_sequence)
+                logging.debug(f"Source: '{source_sequence}', Target: '{target_sequence}'")
+                yield self.text_to_instance(source_sequence, target_sequence)
 
 
     @overrides
@@ -107,3 +138,102 @@ class WritingPromptsDatasetReader(DatasetReader):
             return Instance({"source_tokens": source_field, "target_tokens": target_field})
         else:
             return Instance({'source_tokens': source_field})
+
+
+async def create_dataset_db(dataset_path, db_discriminator, file_path, use_existing_database=True,
+                      sentence_splitter=SpacySentenceSplitter(), batch_size=100, max_workers=8):
+    file_name = os.path.basename(file_path)
+    database_file = f"{dataset_path}/{file_name}_{db_discriminator}.db"
+    dataset_db = f"sqlite:///{database_file}"
+    logging.info(f"Cached dataset path: {dataset_db}")
+
+    # Create dir
+    try:
+        os.makedirs(dataset_path)
+    except OSError:
+        pass
+
+    # Remove database if it shouldn't be reused.
+    if not use_existing_database:
+        try:
+            os.remove(database_file)
+        except OSError:
+            pass
+
+    if not Path(dataset_db).is_file():
+
+        loop = asyncio.get_event_loop()
+
+        f"{dataset_db}?mode=ro&cache=shared"
+        db = dataset.connect(dataset_db, engine_kwargs={"pool_recycle": 3600})
+        db.commit()
+        tasks = []
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+
+            async for lines, story_nums in chunk_stories_from_file(file_path, batch_size=batch_size):
+                story_sentences = sentence_splitter.batch_split_sentences(lines)
+                tasks.append(loop.run_in_executor(executor, SaveStoryToDatabase(dataset_db), story_sentences, story_nums))
+            for task in tasks:
+                story_ids = await task
+                logger.info(f"Saved stories to db with ids: {story_ids}")
+                # Add indices to key fields.
+
+
+            try:
+                db.query('CREATE INDEX idx_story_id on sentence(story_id)')
+            except:
+                pass # Because of the aysnc/
+
+
+    return dataset_db
+
+async def chunk_stories_from_file(file, batch_size=100):
+    """ Async yield batches of stories that are line separated/
+    """
+    line_count = 1
+    lines = []
+    story_nums = []
+    async with AIOFile(file, mode="r", encoding = 'unicode_escape') as f:
+        #for line in f.readline():
+        async for line in LineReader(f):
+            line = line.replace("<newline>","")
+            lines.append(line)
+            story_nums.append(line_count)
+            if len(lines) == batch_size:
+                yield lines, story_nums
+                lines = []
+                story_nums = []
+
+    yield lines, story_nums
+
+
+class SaveStoryToDatabase:
+    def __init__(self, dataset_db):
+        self._dataset_db = dataset_db
+
+    def __call__(self, story_sentences, story_nums):
+        story_ids = []
+        for sentences, story_num in zip(story_sentences, story_nums):
+            db = dataset.connect(self._dataset_db, engine_kwargs={"pool_recycle": 3600})
+            db.begin()
+            try:
+                story_table = db['story']
+                sentence_table = db['sentence']
+                story = dict(story_num=story_num)
+                story_id = story_table.insert(story)
+                sentences_to_save = []
+
+                total_story_tokens = 0
+                for i, sent in enumerate(sentences):
+                    sentence_len = len(sent)
+                    total_story_tokens += sentence_len
+                    sentences_to_save.append(dict(sentence_num=i, text=sent, sentence_len=sentence_len, story_id=story_id))
+                sentence_table.insert_many(sentences_to_save)
+
+                story_table.update(dict(sentence_num=len(sentences), tokens_num=total_story_tokens, id=story_id), ['id'])
+                db.commit()
+                story_ids.append(story_id)
+            except:
+                db.rollback()
+        return story_ids
+
