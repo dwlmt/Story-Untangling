08582977bd6a53399c9f6c2a64b7b99be0f7e541
David Wilmot <david.wilmot@ed.ac.uk>
2019-11-02 16:56:01 +0000

    Update task context and requirements.

--- slurm/predict_batch.sh
+++ slurm/predict_batch.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 #SBATCH -o /home/%u/slurm_logs/slurm-%A_%a.out
 #SBATCH -e /home/%u/slurm_logs/slurm-%A_%a.out
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1  # use 1 GPU
+#SBATCH --mem=20GB  # memory in Mb
 #SBATCH -t 16:00:00  # time requested in hour:minute:seconds
 #SBATCH --cpus-per-task=4  # number of cpus to use - there are 32 on each node.
 
