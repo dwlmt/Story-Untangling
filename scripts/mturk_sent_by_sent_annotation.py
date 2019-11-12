2dc9ecf5a3d7346fc1f9a52c4a5e2021b3dc0ec5
David Wilmot <david.wilmot@ed.ac.uk>
2019-11-12 15:35:01 +0000

    Add a mechanism for excluding the existing counts of annotations already completed.

--- scripts/mturk_sent_by_sent_annotation.py
+++ scripts/mturk_sent_by_sent_annotation.py
@@ -17,6 +17,7 @@ parser.add_argument('--access-key-id', required=True, help="AWS Access Key.")
 parser.add_argument('--secret-access-key', required=True, type=str, help="AWS Access Key.")
 parser.add_argument('--mturk-url', required=False, type=str,
                     default="https://mturk-requester-sandbox.us-east-1.amazonaws.com/", help="Mechanical turk link.")
+parser.add_argument('--existing-counts', required=False, type=str, help="The existing count of story annotations complete. Subtract this from tasks to create.")
 
 args = parser.parse_args()
 
@@ -28,16 +29,16 @@ def create_assignments(args):
     ids_and_codes_df = pandas.read_csv(args["annotations_file"])
 
     worker_requirements = [
+        # Restriction on too many annotations.
+        {
+            'QualificationTypeId': '31I5K8H9C3I6FRRHW1GPDMT1YCESP8',
+            'Comparator': 'DoesNotExist',
+            'ActionsGuarded': 'DiscoverPreviewAndAccept',
+        },
         # Master worker qualification.
         {
             'QualificationTypeId': '2F1QJWKUDD8XADTFD2Q0G6UTO95ALH',
             'Comparator': 'Exists',
-            'ActionsGuarded': 'DiscoverPreviewAndAccept',
-        },
-        # Restriction on too many annotations.
-        {
-            'QualificationTypeId': '3M0XATTAEM8KH2DZ3G7KYLJUFOH59H',
-            'Comparator': 'DoesNotExist',
             'ActionsGuarded': 'DiscoverPreviewAndAccept',
         },
         # Approval is >= 98%
@@ -72,6 +73,13 @@ def create_assignments(args):
     print(f"I have $ ${mturk.get_account_balance()['AvailableBalance']} in MTurk Account")
 
     hit_ids_data = []
+
+    if args["existing_counts"] is not None:
+        existing_counts_df = pandas.read_csv(args["existing_counts"])
+    else:
+        existing_counts_df = None
+
+    total_storys_to_create = 0
     for i, row in ids_and_codes_df.iterrows():
         if i < max_assignments:
             hit_url = f"{args['task_url']}/?mturkCode={row['story_id']}-{row['code']}"
@@ -80,20 +88,36 @@ def create_assignments(args):
             external_question_xml = f'<ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd"><ExternalURL>{hit_url}</ExternalURL><FrameHeight>0</FrameHeight></ExternalQuestion>'
             print(f"Question XML: {external_question_xml}")
 
+            annotations_per_hit = args["annotations_per_hit"]
+
+            if existing_counts_df is not None:
+                story_row = existing_counts_df.loc[existing_counts_df["story_id"] == row['story_id']]
+                if len(story_row) > 0:
+                    annotations_complete = story_row["num_of_workers"]
+                    print(annotations_complete)
+                    annotations_per_hit -= int(annotations_complete)
+
+            print(row['story_id'], annotations_per_hit)
+
+            total_storys_to_create += annotations_per_hit
+
             new_hit = mturk.create_hit(
                 Title=f'Story dramatic tension reading sentence by sentence {row["story_id"]}',
-                Description='Read a short story and record the level of dramatic tension per sentence. Will take 5-10 minutes per hit. Fluent English speakers required. Some violent, sexual or other disturbing content may be present in the stories.',
+                Description='Read a short story and record the level of dramatic tension per sentence. Will take 5-10 minutes per hit. Please do no more than 25 HITs in this batch else they may be rejected and your approval rate impacted. Fluent English speakers required. Some violent, sexual or other disturbing content may be present in the stories.',
                 Keywords='story, narrative, storytelling, annotation, research, nlp, reading',
                 Reward=f'{args["reward"]}',
-                MaxAssignments=args["annotations_per_hit"],
-                LifetimeInSeconds=604800,  # One week
+                MaxAssignments=annotations_per_hit,
+                LifetimeInSeconds=1209600,  # Two weeks
                 AssignmentDurationInSeconds=3600,  # One hour
-                AutoApprovalDelayInSeconds=432000,  # 5 Days
+                AutoApprovalDelayInSeconds=604800,  # 5 Days
                 Question=external_question_xml,
                 QualificationRequirements=worker_requirements
             )
 
             hit_ids_data.append({"hit_id": new_hit['HIT']['HITId'], "story_id": row['story_id'], "code": row['code']})
+
+
+    print(total_storys_to_create)
 
     hit_df = pandas.DataFrame(data=hit_ids_data)
     hit_df.to_csv(args["output_file"])
